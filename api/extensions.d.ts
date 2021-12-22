11fa7d303f7e7c1d136d6740ca2951475cab36f3
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-12-22 22:11:36 +0700

    feat: Add more environment variables

--- api/extensions.d.ts
+++ api/extensions.d.ts
@@ -3,10 +3,20 @@ declare global {
     interface ProcessEnv {
       COOKIE_SECRET?: string;
       DATABASE_URL: string;
+      JWT_AUDIENCE?: string;
+      JWT_ISSUER?: string;
+      JWT_PRIVATE_KEY: string;
+      JWT_PUBLIC_KEY: string;
       NODE_ENV?: 'production' | 'development';
       PORT?: number;
       TOTP_ISSUER?: string;
     }
+  }
+}
+
+declare module 'express' {
+  interface Request {
+    userID?: string;
   }
 }
 
