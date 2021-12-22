b197b25ba66b0136dcbfacc1512bac7ca255aaff
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-12-22 10:41:46 +0700

    feat: Use Prisma to generate database schema

---
+++ api/prisma/migrations/20211222033534_init/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `userPK` INTEGER NOT NULL AUTO_INCREMENT,
+    `userID` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `phoneNumber` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `totpSecret` VARCHAR(191) NOT NULL,
+    `fullName` VARCHAR(191) NOT NULL,
+    `updatedAt` DATETIME(3) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `User_username_key`(`username`),
+    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_phoneNumber_key`(`phoneNumber`),
+    PRIMARY KEY (`userPK`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Attendance` (
+    `attendancePK` INTEGER NOT NULL AUTO_INCREMENT,
+    `attendanceID` VARCHAR(191) NOT NULL,
+    `timeEnter` DATETIME(3) NOT NULL,
+    `ipAddressEnter` VARCHAR(191) NOT NULL,
+    `deviceEnter` VARCHAR(191) NOT NULL,
+    `remarksEnter` VARCHAR(191) NOT NULL,
+    `timeLeave` DATETIME(3) NULL,
+    `ipAddressLeave` VARCHAR(191) NULL,
+    `deviceLeave` VARCHAR(191) NULL,
+    `remarksLeave` VARCHAR(191) NULL,
+    `userPK` INTEGER NOT NULL,
+
+    PRIMARY KEY (`attendancePK`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Attendance` ADD CONSTRAINT `Attendance_userPK_fkey` FOREIGN KEY (`userPK`) REFERENCES `User`(`userPK`) ON DELETE RESTRICT ON UPDATE CASCADE;
