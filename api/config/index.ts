894f07e85f685e39c36177780e96d350ed2b3146
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-12-22 10:57:19 +0700

    feat: Ensure environment variables are set up properly

--- api/config/index.ts
+++ api/config/index.ts
@@ -1,7 +1,28 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+// Allow environment variables from a file for development.
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
+
+/**
+ * Returns value stored in environment variable with the given 'name'.
+ * Throws Error if no such variable or if variable undefined; thus ensuring that the variable exists.
+ *
+ * @param variable - A variable to check whether it exists or not.
+ */
+const env = (variable: string) => {
+  if (!variable) {
+    throw new Error(`Missing core environment variable: '${variable}'.`);
+  }
+
+  return variable;
+};
+
 /**
  * All configuration values of this application from the environment variables.
  */
 const config = {
+  DATABASE: env(process.env.DATABASE_URL),
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 8080,
 };
--- api/extensions.d.ts
+++ api/extensions.d.ts
@@ -1,6 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      DATABASE_URL: string;
       NODE_ENV?: 'production' | 'development';
       PORT?: number;
     }
