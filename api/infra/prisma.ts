3778b6535dddb26c69c95c0a3b71ee20af7dd339
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-12-27 15:58:39 +0700

    feat: Reduce logging for convenience in Prisma

--- api/infra/prisma.ts
+++ api/infra/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({ log: ['query', 'info', 'warn', 'error'] });
+const prisma = new PrismaClient({ log: ['info', 'warn', 'error'] });
 
 export default prisma;
