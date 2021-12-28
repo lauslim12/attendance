41b55b71bfb6402a5927f1b68540e920b29ea143
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2021-12-28 11:50:20 +0700

    feat: Add environment variables for Mailtrap and Nodemailer

--- api/extensions.d.ts
+++ api/extensions.d.ts
@@ -3,10 +3,18 @@ declare global {
     interface ProcessEnv {
       COOKIE_SECRET?: string;
       DATABASE_URL: string;
+      EMAIL_FROM?: string;
+      EMAIL_HOST: string;
+      EMAIL_USERNAME: string;
+      EMAIL_PASSWORD: string;
+      EMAIL_PORT?: number;
       JWT_AUDIENCE?: string;
       JWT_ISSUER?: string;
       JWT_PRIVATE_KEY: string;
       JWT_PUBLIC_KEY: string;
+      MAILTRAP_HOST: string;
+      MAILTRAP_USERNAME: string;
+      MAILTRAP_PASSWORD: string;
       NODE_ENV?: 'production' | 'development';
       PORT?: number;
       TOTP_ISSUER?: string;
