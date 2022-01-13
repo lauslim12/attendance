55ef5c85186d96d3a8b1c120070812d37b82d219
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2022-01-13 22:29:41 +0700

    refactor: Slight refactor on 'User' entity, make 'Where' easier to manipulate

--- api/modules/user/controller.ts
+++ api/modules/user/controller.ts
@@ -29,24 +29,27 @@ const UserController = {
   },
 
   /**
-   * Creates a single user.
+   * Creates a single user. Has several validations to ensure that the username,
+   * email, and phone number are all unique and have not yet been used by another user.
    *
    * @param _ - Express.js's request object.
    * @param res - Express.js's response object.
    * @param next - Express.js's next function.
    */
   createUser: async (req: Request, res: Response, next: NextFunction) => {
-    if (await UserService.getUserByUsername(req.body.username)) {
+    const { username, email, phoneNumber, password, fullName } = req.body;
+
+    if (await UserService.getUser({ username })) {
       next(new AppError('This username has existed already!', 400));
       return;
     }
 
-    if (await UserService.getUserByEmail(req.body.email)) {
+    if (await UserService.getUser({ email })) {
       next(new AppError('This email has been used by another user!', 400));
       return;
     }
 
-    if (await UserService.getUserByPhoneNumber(req.body.phoneNumber)) {
+    if (await UserService.getUser({ phoneNumber })) {
       next(
         new AppError('This phone number has been used by another user!', 400)
       );
@@ -54,12 +57,12 @@ const UserController = {
     }
 
     const user = await UserService.createUser({
-      username: req.body.username,
-      email: req.body.email,
-      phoneNumber: req.body.phoneNumber,
-      password: req.body.password,
+      username,
+      email,
+      phoneNumber,
+      password,
       totpSecret: '', // kept blank to ensure that this gets filled in the service layer
-      fullName: req.body.fullName,
+      fullName,
     });
 
     sendResponse({
@@ -80,7 +83,7 @@ const UserController = {
    * @param res - Express.js's response object.
    */
   getUser: async (req: Request, res: Response) => {
-    const user = await UserService.getUserByID(req.params.id);
+    const user = await UserService.getUser({ userID: req.params.id });
 
     sendResponse({
       req,
@@ -94,40 +97,47 @@ const UserController = {
   },
 
   /**
-   * Updates a single user.
+   * Updates a single user. Has validations to ensure that the current user
+   * does not use others phone number or email.
    *
    * @param req - Express.js's request object.
    * @param res - Express.js's response object.
    * @param next - Express.js's next function.
    */
   updateUser: async (req: Request, res: Response, next: NextFunction) => {
-    const { email, phoneNumber } = req.body;
+    const { email, phoneNumber, password, fullName } = req.body;
     const { id } = req.params;
 
-    const currentUser = await UserService.getUserByID(id);
+    const currentUser = await UserService.getUser({ userID: id });
     if (!currentUser) {
       next(new AppError('The user with this ID does not exist!', 404));
       return;
     }
 
-    if (email) {
-      const userByEmail = await UserService.getUserByEmail(email);
-      if (userByEmail && userByEmail.userID !== currentUser.userID) {
-        next(new AppError('This email has been used by another user!', 400));
-        return;
-      }
+    const userByEmail = email ? await UserService.getUser({ email }) : null;
+    if (userByEmail && userByEmail.userID !== currentUser.userID) {
+      next(new AppError('This email has been used by another user!', 400));
+      return;
     }
 
-    if (phoneNumber) {
-      const userByPhone = await UserService.getUserByPhoneNumber(phoneNumber);
-      if (userByPhone && userByPhone.userID !== currentUser.userID) {
-        next(new AppError('This number has been used by another user!', 400));
-        return;
-      }
+    const userByPhone = phoneNumber
+      ? await UserService.getUser({ phoneNumber })
+      : null;
+    if (userByPhone && userByPhone.userID !== currentUser.userID) {
+      next(new AppError('This number has been used by another user!', 400));
+      return;
     }
 
     // everything is optional and sanitized according to the previous validation layer
-    const user = await UserService.updateUser(req.params.id, req.body);
+    const user = await UserService.updateUser(
+      { userID: id },
+      {
+        email,
+        phoneNumber,
+        fullName,
+        password,
+      }
+    );
 
     sendResponse({
       req,
@@ -148,12 +158,14 @@ const UserController = {
    * @param next - Express.js's next function.
    */
   deleteUser: async (req: Request, res: Response, next: NextFunction) => {
-    if (!(await UserService.getUserByID(req.params.id))) {
+    const { id } = req.params;
+
+    if (!(await UserService.getUser({ userID: id }))) {
       next(new AppError('The user with this ID does not exist!', 404));
       return;
     }
 
-    await UserService.deleteUser(req.params.id);
+    await UserService.deleteUser({ userID: id });
 
     res.status(204).send();
   },
