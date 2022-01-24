faed9861ee590cc5f2f2fd6131dd2d7164ff4203
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2022-01-24 13:19:28 +0700

    feat: Create client-side protected route HOC

---
+++ web/components/AuthRoute.tsx
@@ -0,0 +1,41 @@
+import { useRouter } from 'next/router';
+import type { ReactNode } from 'react';
+import { useEffect } from 'react';
+
+import { useMe } from '../utils/hooks';
+import routes from '../utils/routes';
+import Spinner from './Spinner';
+
+/**
+ * Props.
+ */
+type Props = {
+  children: ReactNode;
+};
+
+/**
+ * A middleware, top-level component that prevents non-logged in users to access certain routes
+ * in the client-side. Quite simple, if the user does not exist in the context / SWR,
+ * then redirect to login screen.
+ *
+ * @param params - Props.
+ * @returns React Functional Component
+ */
+const AuthRoute = ({ children }: Props) => {
+  const { data, isLoading } = useMe();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isLoading && !data.status?.isAuthenticated) {
+      router.replace(routes.notAuthorized);
+    }
+  }, [isLoading, data.status, router]);
+
+  if (isLoading || !data.status?.isAuthenticated) {
+    return <Spinner />;
+  }
+
+  return <>{children}</>;
+};
+
+export default AuthRoute;
