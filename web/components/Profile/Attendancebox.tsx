40079da7b7abe10a75ba4ad934c525cd3e6b29fa
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2022-01-28 22:37:28 +0700

    feat: Redesign attendance box

--- web/components/Profile/Attendancebox.tsx
+++ web/components/Profile/Attendancebox.tsx
@@ -1,15 +1,16 @@
 import {
-  Box,
+  Badge,
+  Grid,
   Heading,
-  Table,
-  Tbody,
-  Td,
+  HStack,
+  List,
+  ListIcon,
+  ListItem,
   Text,
-  Th,
-  Thead,
-  Tr,
+  useMediaQuery,
   VStack,
 } from '@chakra-ui/react';
+import { FaFlagCheckered, FaHourglassStart } from 'react-icons/fa';
 import { memo } from 'react';
 
 import type { Attendance } from '../../utils/types';
@@ -30,6 +31,106 @@ const formatWorkingHours = (end: string, start: string) => {
 };
 
 /**
+ * Ensures that date is in the proper format.
+ *
+ * @param date - Date in the string format.
+ * @param type - Type of the wished format.
+ * @returns Formatted date.
+ */
+const formatDate = (date: string, type: 'complete' | 'month' | 'day') => {
+  if (type === 'complete') {
+    return new Date(date).toLocaleString('en-GB', {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    });
+  }
+
+  if (type === 'month') {
+    return new Date(date).toLocaleString('en-GB', { month: 'short' });
+  }
+
+  return new Date(date).toLocaleString('en-GB', { day: 'numeric' });
+};
+
+/**
+ * Attendance card.
+ *
+ * @param params - Props.
+ * @returns React functional component.
+ */
+const AttendanceCard = ({ attendance }: { attendance: Attendance }) => {
+  const [largerThan1280] = useMediaQuery('(min-width: 1280px)');
+
+  return (
+    <VStack
+      as="article"
+      align="start"
+      border="1px dashed #bb48bf"
+      borderRadius="md"
+      p={3}
+    >
+      <HStack align="center">
+        <VStack p={2}>
+          <Heading as="p" size="lg" color="pink.400">
+            {formatDate(attendance.timeEnter, 'day')}
+          </Heading>
+
+          <Heading as="p" size="md">
+            {formatDate(attendance.timeEnter, 'month')}
+          </Heading>
+
+          {attendance.timeLeave && (
+            <Badge colorScheme="linkedin">
+              {formatWorkingHours(attendance.timeLeave, attendance.timeEnter)}
+            </Badge>
+          )}
+        </VStack>
+
+        <VStack align="start" px={2} fontSize="sm" w="full">
+          <List spacing={2}>
+            <ListItem>
+              <ListIcon as={FaHourglassStart} />
+              Clocked in at {formatDate(
+                attendance.timeEnter,
+                'complete'
+              )} with {attendance.deviceEnter} ({attendance.ipAddressEnter}).
+            </ListItem>
+
+            {attendance.remarksEnter && largerThan1280 && (
+              <ListItem fontSize="xs" fontStyle="italic">
+                Remarks: {attendance.remarksEnter}
+              </ListItem>
+            )}
+
+            {attendance.timeLeave ? (
+              <>
+                <ListItem>
+                  <ListIcon as={FaFlagCheckered} />
+                  Clocked out at {formatDate(
+                    attendance.timeLeave,
+                    'complete'
+                  )}{' '}
+                  with {attendance.deviceLeave} ({attendance.ipAddressLeave}).
+                </ListItem>
+
+                {attendance.remarksLeave && largerThan1280 && (
+                  <ListItem fontSize="xs" fontStyle="italic">
+                    Remarks: {attendance.remarksLeave}
+                  </ListItem>
+                )}
+              </>
+            ) : (
+              <ListItem>You have not yet checked out!</ListItem>
+            )}
+          </List>
+        </VStack>
+      </HStack>
+    </VStack>
+  );
+};
+
+/**
  * Attendance box to show attendances.
  *
  * @param params - Props.
@@ -42,49 +143,20 @@ const Attendancebox = ({ attendances }: { attendances: Attendance[] }) => (
     </Heading>
     <Text textAlign="center">All of your attendances in this system.</Text>
 
-    <Box overflowX="auto" w="full">
-      <Table variant="striped">
-        <Thead>
-          <Tr>
-            <Th>Date (Enter)</Th>
-            <Th>IP and Device (Enter)</Th>
-            <Th>Remarks (Enter)</Th>
-            <Th>Date (Leave)</Th>
-            <Th>IP and Device (Leave)</Th>
-            <Th>Remarks (Leave)</Th>
-            <Th>Working Hours</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {attendances.map((attendance) => (
-            <Tr key={attendance.attendanceID}>
-              <Td>{new Date(attendance.timeEnter).toLocaleString('en-GB')}</Td>
-              <Td>
-                {attendance.ipAddressEnter} - {attendance.deviceEnter}
-              </Td>
-              <Td>{attendance.remarksEnter}</Td>
-              <Td>
-                {attendance.timeLeave
-                  ? new Date(attendance.timeLeave).toLocaleString('en-GB')
-                  : null}
-              </Td>
-              <Td>
-                {attendance.ipAddressLeave} - {attendance.deviceLeave}
-              </Td>
-              <Td>{attendance.remarksLeave}</Td>
-              <Td>
-                {attendance.timeLeave
-                  ? formatWorkingHours(
-                      attendance.timeLeave,
-                      attendance.timeEnter
-                    )
-                  : null}
-              </Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </Box>
+    {attendances.length ? (
+      <Grid templateColumns={['1fr', 'repeat(2, 1fr)']} gap={3} w="full" mt={5}>
+        {attendances.map((attendance) => (
+          <AttendanceCard
+            key={attendance.attendanceID}
+            attendance={attendance}
+          />
+        ))}
+      </Grid>
+    ) : (
+      <Text fontSize="sm" textAlign="center">
+        No attendances data found in the system. Try to check in!
+      </Text>
+    )}
   </VStack>
 );
 
