7d03ddcaf993b42e4690b2938344fda4e7617f2d
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2022-02-02 11:48:24 +0700

    feat: Improve default logger

--- api/modules/middleware/logger.ts
+++ api/modules/middleware/logger.ts
@@ -3,21 +3,57 @@ import expressWinston from 'express-winston';
 import path from 'path';
 import winston from 'winston';
 
+import getDeviceID from '../../util/device-id';
+
 /**
  * Options for the default loggers.
  */
 const options: LoggerOptions = {
+  // Store all logs in files. Will be created automatically as long as it does not break any permissions.
   transports: [
     new winston.transports.File({
-      filename: path.join(__dirname, '..', '..', 'logs', 'attendance.log'), // Will be created automatically as long as it does not break any permissions.
+      filename: path.join(__dirname, '..', '..', 'logs', 'attendance.log'),
     }),
   ],
-  format: winston.format.json(),
+
+  // Prints all logs with timestamps in JSON format.
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+
+  // Print request metadata.
   meta: true,
+
+  // Inject additional object: True IP and device if applicable.
+  dynamicMeta: (req) => ({ deviceInfo: getDeviceID(req) }),
+
+  // Customized message attribute in logging.
   msg: 'HTTP {{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}',
+
+  // Deactivate both colorization and Express Format, we have our own format.
   expressFormat: false,
   colorize: false,
+
+  // Do not allow cookie and authorization in our loggers. It may have session ID or MFA token.
+  headerBlacklist: ['authorization', 'cookie'],
+
+  // Ignore frequently called routes via SWR.
   ignoredRoutes: ['/api/v1/auth/status', '/api/v1/attendance/status'],
+
+  // Log everything.
+  requestWhitelist: [
+    'url',
+    'headers',
+    'method',
+    'httpVersion',
+    'originalUrl',
+    'query',
+    'body',
+  ],
+
+  // Do not log passwords.
+  bodyBlacklist: ['password'],
 };
 
 /**
