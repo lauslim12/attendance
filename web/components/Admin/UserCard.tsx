6cbb6eef395378826fd5926ee0120f9e4d9d4977
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2022-02-03 21:48:37 +0700

    feat: Create baseline admin panel

---
+++ web/components/Admin/UserCard.tsx
@@ -0,0 +1,163 @@
+import {
+  Badge,
+  Button,
+  HStack,
+  Text,
+  useColorModeValue,
+  useToast,
+  VStack,
+} from '@chakra-ui/react';
+import axios from 'axios';
+import dynamic from 'next/dynamic';
+import { memo, useState } from 'react';
+
+import { useUsers } from '../../utils/hooks';
+import type { User } from '../../utils/types';
+import { FailedToast, SuccessToast } from '../Toast';
+
+/**
+ * Dynamic imports.
+ */
+const AlertOverlay = dynamic(() => import('../Overlay/AlertOverlay'));
+const EditUserModal = dynamic(() => import('../Overlay/EditUserModal'));
+
+/**
+ * Props.
+ */
+type Props = {
+  user: User;
+};
+
+/**
+ * Card that is filled with User data.
+ *
+ * @param params - Props.
+ * @returns User card component as React functional component.
+ */
+const UserCard = ({ user }: Props) => {
+  const { mutate } = useUsers();
+  const [isOpenDeactivate, setIsOpenDeactivate] = useState(false);
+  const [isOpenEdit, setIsOpenEdit] = useState(false);
+  const [isOpenReactivate, setIsOpenReactivate] = useState(false);
+  const toast = useToast();
+
+  const deactivateUser = () => {
+    axios({
+      method: 'PATCH',
+      url: `/api/v1/users/${user.userID}`,
+      data: { isActive: false },
+    })
+      .then(() => SuccessToast(toast, 'Successfully deactivated user.'))
+      .catch((err) => FailedToast(toast, err.message))
+      .finally(() => {
+        setIsOpenDeactivate(false);
+        mutate();
+      });
+  };
+
+  const reactivateUser = () => {
+    axios({
+      method: 'PATCH',
+      url: `/api/v1/users/${user.userID}`,
+      data: { isActive: true },
+    })
+      .then(() => SuccessToast(toast, 'Successfully reactivated user.'))
+      .catch((err) => FailedToast(toast, err.message))
+      .finally(() => {
+        setIsOpenReactivate(false);
+        mutate();
+      });
+  };
+
+  return (
+    <>
+      <AlertOverlay
+        isOpen={isOpenDeactivate}
+        setIsOpen={setIsOpenDeactivate}
+        title="Deactivate User"
+        description={`Are you sure to deactivate the user ${user.fullName}?`}
+        fn={deactivateUser}
+      />
+
+      <AlertOverlay
+        isOpen={isOpenReactivate}
+        setIsOpen={setIsOpenReactivate}
+        title="Reactivate User"
+        description={`Are you sure to reactivate the user ${user.fullName}?`}
+        fn={reactivateUser}
+      />
+
+      <EditUserModal
+        isOpen={isOpenEdit}
+        onClose={() => setIsOpenEdit(false)}
+        user={user}
+      />
+
+      <VStack
+        as="article"
+        align="stretch"
+        p={4}
+        borderRadius="md"
+        bg={useColorModeValue('white', 'gray.900')}
+        borderWidth={1}
+        borderColor={useColorModeValue('gray.200', 'gray.700')}
+        transition=".2s"
+      >
+        <VStack spacing={1}>
+          <Text fontSize="sm" fontWeight="bold">
+            {user.fullName}
+          </Text>
+          <Text fontSize="xs">{user.email}</Text>
+          <Text fontSize="xs">{user.phoneNumber}</Text>
+        </VStack>
+
+        <VStack spacing={1} fontSize="xs">
+          <Text fontWeight="bold">Role/Status:</Text>
+          <HStack>
+            <Badge colorScheme={user.role === 'admin' ? 'green' : 'blue'}>
+              {user.role}
+            </Badge>
+
+            <Badge colorScheme={user.isActive ? 'orange' : 'red'}>
+              {user.isActive ? 'Active' : 'Inactive'}
+            </Badge>
+          </HStack>
+        </VStack>
+
+        <VStack fontSize="xs" w="full">
+          <Text fontWeight="bold">Actions:</Text>
+          <Button
+            size="xs"
+            colorScheme="linkedin"
+            w="full"
+            onClick={() => setIsOpenEdit(true)}
+          >
+            Edit
+          </Button>
+
+          {user.isActive ? (
+            <Button
+              size="xs"
+              colorScheme="red"
+              w="full"
+              onClick={() => setIsOpenDeactivate(true)}
+            >
+              Deactivate
+            </Button>
+          ) : (
+            <Button
+              size="xs"
+              colorScheme="purple"
+              w="full"
+              onClick={() => setIsOpenReactivate(true)}
+            >
+              Reactivate
+            </Button>
+          )}
+        </VStack>
+      </VStack>
+    </>
+  );
+};
+
+export default memo(UserCard);
---
+++ web/components/Overlay/EditUserModal.tsx
@@ -0,0 +1,152 @@
+import {
+  Alert,
+  AlertIcon,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useToast,
+  VStack,
+} from '@chakra-ui/react';
+import type { FormEvent } from 'react';
+import { memo, useState } from 'react';
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
+
+import { useStatusAndUser, useUsers } from '../../utils/hooks';
+import axios from '../../utils/http';
+import type { User } from '../../utils/types';
+import SelectInput from '../Input/SelectInput';
+import TextInput from '../Input/TextInput';
+import { FailedToast, SuccessToast } from '../Toast';
+
+/**
+ * Props.
+ */
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  user: User;
+};
+
+/**
+ * Spawns a modal to edit a user.
+ *
+ * @param params - Props.
+ * @returns Edit modal functional component.
+ */
+const EditUserModal = ({ isOpen, onClose, user }: Props) => {
+  const { mutate: setUsers } = useUsers();
+  const { mutate: setStatus } = useStatusAndUser();
+  const [email, setEmail] = useState(user.email);
+  const [fullName, setFullName] = useState(user.fullName);
+  const [phoneNumber, setPhoneNumber] = useState(user.phoneNumber);
+  const [role, setRole] = useState(user.role as string);
+
+  // Reactive component.
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+
+  // Edit a user.
+  const editUser = (e: FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    axios({
+      method: 'PATCH',
+      url: `/api/v1/users/${user.userID}`,
+      data: { email, fullName, phoneNumber, role },
+    })
+      .then((res) => SuccessToast(toast, res.message))
+      .catch((err) => FailedToast(toast, err.message))
+      .finally(() => {
+        setUsers();
+        setStatus();
+        setIsLoading(false);
+        onClose();
+      });
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      size="xl"
+      motionPreset="slideInBottom"
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Edit User</ModalHeader>
+        <ModalCloseButton />
+
+        <ModalBody>
+          <VStack as="form" align="stretch" onSubmit={editUser}>
+            <Alert status="success" variant="left-accent">
+              <AlertIcon />
+              Edit a user here!
+            </Alert>
+
+            <TextInput
+              label="Full name"
+              placeholder="John Smith"
+              value={fullName}
+              setValue={setFullName}
+              helper="The full name to be used."
+            />
+
+            <TextInput
+              label="Email"
+              placeholder="john.smith@gmail.com"
+              value={email}
+              setValue={setEmail}
+              helper="The email to be used."
+            />
+
+            <TextInput
+              label="Phone number"
+              placeholder="+62-890-1122-3344"
+              value={phoneNumber}
+              setValue={setPhoneNumber}
+              helper="The phone number to be used."
+            />
+
+            <SelectInput
+              label="Role"
+              value={role}
+              setValue={setRole}
+              options={['admin', 'user']}
+              helper="Role of this user"
+            />
+          </VStack>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button
+            leftIcon={<FaPencilAlt />}
+            colorScheme="green"
+            mr={3}
+            isLoading={isLoading}
+            onClick={editUser}
+            disabled={
+              email.trim() === '' ||
+              fullName.trim() === '' ||
+              phoneNumber.trim() === '' ||
+              role.trim() === '' ||
+              isLoading
+            }
+          >
+            Submit
+          </Button>
+          <Button leftIcon={<FaTimes />} colorScheme="red" onClick={onClose}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default memo(EditUserModal);
