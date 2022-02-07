6ed02e962800c08b6736250d3f3b39e8e5c6023b
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2022-02-07 21:04:11 +0700

    feat: Return '406 Not Acceptable' on invalid 'Accept' headers

--- api/modules/middleware/body-parser.ts
+++ api/modules/middleware/body-parser.ts
@@ -1,5 +1,5 @@
 import type { NextFunction, Request, Response } from 'express';
-import { json } from 'express';
+import { json as parse } from 'express';
 
 import AppError from '../../util/app-error';
 
@@ -14,7 +14,21 @@ import AppError from '../../util/app-error';
  * @returns Customized instance of `express.json`, complete with preprocessing.
  */
 const bodyParser = (req: Request, res: Response, next: NextFunction) => {
-  const { ['content-type']: type, ['content-length']: length } = req.headers;
+  const {
+    ['content-type']: type,
+    ['content-length']: length,
+    accept,
+  } = req.headers;
+
+  // Declare allowed 'Accept' and 'Content-Type'.
+  const json = 'application/json';
+  const vendor = 'application/vnd.nicholasdw.v1+json';
+
+  // If the request do not `Accept` available formats, deny the request.
+  if (!accept?.includes(json) && !accept?.includes(vendor)) {
+    next(new AppError('API does not accept the requested content type.', 406));
+    return;
+  }
 
   // Ensures 'Content-Type' is 'application/json'.
   if (!type?.includes('application/json')) {
@@ -39,7 +53,7 @@ const bodyParser = (req: Request, res: Response, next: NextFunction) => {
 
   // We have to `return` this specific middleware as this is an Express middleware, so it can go
   // straight to the next stack. Simply calling the `json` function will not suffice.
-  return json({ type: 'application/json', limit: 512 })(req, res, next);
+  return parse({ type: json, limit: 512 })(req, res, next);
 };
 
 export default bodyParser;
