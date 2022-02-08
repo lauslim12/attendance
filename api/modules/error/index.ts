a1490cdb884e95ab3d8959da03c333f8c5dd568f
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2022-02-08 14:50:23 +0700

    fix: Allow fallback on critical errors

--- api/modules/error/index.ts
+++ api/modules/error/index.ts
@@ -8,18 +8,36 @@ import AppError from '../../util/app-error';
 import sendError from '../../util/send-error';
 
 /**
+ * Sends an API error response to the requesting client. In development
+ * phase, the API will send back `stack` property in the response.
+ *
+ * @param req - Express.js's request object.
+ * @param res - Express.js's response object.
+ * @param err - Error object in the form of `AppError`.
+ */
+const sendErrorResponse = (req: Request, res: Response, err: AppError) => {
+  if (config.NODE_ENV === 'development') {
+    sendError({ req, res, error: err, stack: err.stack });
+    return;
+  }
+
+  sendError({ req, res, error: err });
+};
+
+/**
  * Handles errors for expected errors. Transforms most error objects into a readable format
  * which is `AppError`.
  *
- * @param err - The 'true' error which was thrown.
+ * @param err - The 'true' error which was thrown, casted to `any` as errors can take up many forms.
  * @returns A new error object that conforms to 'AppError'.
  */
-const handleOperationalErrors = (err: AppError) => {
+const handleOperationalErrors = (err: Error) => {
   // Handle body parser errors.
   if (err instanceof SyntaxError) {
     return new AppError('Invalid JSON! Please insert a valid one.', 400);
   }
 
+  // @ts-expect-error: Some errors may have this `type` property!
   if (err.type === 'entity.too.large') {
     return new AppError('Request too large! Please reduce your payload!', 413);
   }
@@ -64,8 +82,10 @@ const handleOperationalErrors = (err: AppError) => {
     return new AppError('MFA session expired. Please log in again!', 401);
   }
 
-  // If anything is not caught, handle them here.
-  return new AppError(err.message, err.statusCode);
+  // If anything is not caught, then it is most likely an unknown error. We will
+  // print the error to standard output and send back a generic `500` error.
+  console.error(err);
+  return new AppError('Internal server error!', 500);
 };
 
 /**
@@ -82,35 +102,29 @@ const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  // `AppError` is always an expected error.
+  // Catch expected errors.
+  if (err instanceof AppError) {
+    sendErrorResponse(req, res, err);
+    return;
+  }
+
+  // Fallback to this error if error is not an `AppError`. We will try
+  // to check if it is an operational error or not.
   if (err instanceof Error) {
-    // Force cast as this is something that we would want.
-    const error = handleOperationalErrors(err as AppError);
-
-    if (config.NODE_ENV === 'development') {
-      sendError({ req, res, error, stack: err.stack });
-    } else {
-      sendError({ req, res, error });
-    }
-
+    sendErrorResponse(req, res, handleOperationalErrors(err));
     return;
   }
 
   // Otherwise, it is unexpected and should be handled properly in both environments by
   // transforming them into an instance of `AppError` and printing the stack trace properly.
+  const error = new AppError('Unknown error! Please try again later!', 500);
+
+  // Print both errors to standard output. This is considered a critical failure.
   console.error(err);
-  const error = new AppError(
-    'An unknown error occured! Please try again later!',
-    500
-  );
   console.error(error);
 
   // Send stack in development, do not send stack in production.
-  if (config.NODE_ENV === 'development') {
-    sendError({ req, res, error, stack: error.stack });
-  } else {
-    sendError({ req, res, error });
-  }
+  sendErrorResponse(req, res, error);
 
   // Go next.
   next();
