9bb72ff272bc29aa6a586203b0e84bdc20d3807c
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2022-02-11 12:25:07 +0700

    fix: Adjust component to use Grid for equal spacing

--- web/components/Card/AttendanceCard.tsx
+++ web/components/Card/AttendanceCard.tsx
@@ -1,7 +1,7 @@
 import {
   Badge,
+  Grid,
   Heading,
-  HStack,
   List,
   ListIcon,
   ListItem,
@@ -69,77 +69,79 @@ const AttendanceCard = ({ attendance, withName = false }: Props) => {
   const [largerThan1280] = useMediaQuery('(min-width: 1280px)');
 
   return (
-    <VStack
+    <Grid
       as="article"
-      align="start"
+      templateColumns="75px 1fr"
       border="1px dashed #95a5a6"
       borderRadius="md"
       p={3}
     >
-      <HStack align="center">
-        <VStack p={2}>
-          <Heading as="p" size="lg">
-            {formatDate(attendance.timeEnter, 'day')}
-          </Heading>
+      <VStack p={2} justify="center">
+        <Heading as="p" size="lg">
+          {formatDate(attendance.timeEnter, 'day')}
+        </Heading>
 
-          <Heading as="p" size="md">
-            {formatDate(attendance.timeEnter, 'month')}
-          </Heading>
+        <Heading as="p" size="md">
+          {formatDate(attendance.timeEnter, 'month')}
+        </Heading>
 
-          {attendance.timeLeave && (
-            <Badge colorScheme="linkedin">
-              {formatWorkingHours(attendance.timeLeave, attendance.timeEnter)}
-            </Badge>
-          )}
-        </VStack>
+        {attendance.timeLeave ? (
+          <Badge fontSize="xx-small" colorScheme="linkedin">
+            {formatWorkingHours(attendance.timeLeave, attendance.timeEnter)}
+          </Badge>
+        ) : (
+          <Badge fontSize="xx-small" colorScheme="orange">
+            Pending
+          </Badge>
+        )}
+      </VStack>
 
-        <VStack align="start" px={2} fontSize="sm" w="full">
-          <List spacing={2}>
-            {withName && (
-              <ListItem>
-                <ListIcon as={FaChild} />
-                {attendance.user.fullName}
-              </ListItem>
-            )}
-
+      <VStack align="start" px={2} fontSize="sm">
+        <List spacing={2}>
+          {withName && (
             <ListItem>
-              <ListIcon as={FaHourglassStart} />
-              Clocked in at {formatDate(
-                attendance.timeEnter,
-                'complete'
-              )} with {attendance.deviceEnter} ({attendance.ipAddressEnter}).
+              <ListIcon as={FaChild} />
+              {attendance.user.fullName}
             </ListItem>
+          )}
 
-            {attendance.remarksEnter && largerThan1280 && (
-              <ListItem fontSize="xs" fontStyle="italic">
-                Remarks: {attendance.remarksEnter}
+          <ListItem>
+            <ListIcon as={FaHourglassStart} />
+            Clocked in at {formatDate(
+              attendance.timeEnter,
+              'complete'
+            )} with {attendance.deviceEnter} ({attendance.ipAddressEnter}).
+          </ListItem>
+
+          {attendance.remarksEnter && largerThan1280 && (
+            <ListItem fontSize="xs" fontStyle="italic">
+              Remarks: {attendance.remarksEnter}
+            </ListItem>
+          )}
+
+          {attendance.timeLeave ? (
+            <>
+              <ListItem>
+                <ListIcon as={FaFlagCheckered} />
+                Clocked out at {formatDate(
+                  attendance.timeLeave,
+                  'complete'
+                )}{' '}
+                with {attendance.deviceLeave} ({attendance.ipAddressLeave}).
               </ListItem>
-            )}
 
-            {attendance.timeLeave ? (
-              <>
-                <ListItem>
-                  <ListIcon as={FaFlagCheckered} />
-                  Clocked out at {formatDate(
-                    attendance.timeLeave,
-                    'complete'
-                  )}{' '}
-                  with {attendance.deviceLeave} ({attendance.ipAddressLeave}).
+              {attendance.remarksLeave && largerThan1280 && (
+                <ListItem fontSize="xs" fontStyle="italic">
+                  Remarks: {attendance.remarksLeave}
                 </ListItem>
-
-                {attendance.remarksLeave && largerThan1280 && (
-                  <ListItem fontSize="xs" fontStyle="italic">
-                    Remarks: {attendance.remarksLeave}
-                  </ListItem>
-                )}
-              </>
-            ) : (
-              <ListItem>Not yet checked out!</ListItem>
-            )}
-          </List>
-        </VStack>
-      </HStack>
-    </VStack>
+              )}
+            </>
+          ) : (
+            <ListItem>Not yet checked out!</ListItem>
+          )}
+        </List>
+      </VStack>
+    </Grid>
   );
 };
 
