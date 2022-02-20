ffcf17cd35ca5208278ccf20aaa9f04de84283e1
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2022-02-20 09:28:32 +0700

    refactor: Generalize text input component

--- web/components/Input/TextInput.tsx
+++ web/components/Input/TextInput.tsx
@@ -2,18 +2,25 @@ import {
   FormControl,
   FormHelperText,
   FormLabel,
+  Icon,
   Input,
+  InputGroup,
+  InputLeftElement,
 } from '@chakra-ui/react';
-import type { Dispatch, SetStateAction } from 'react';
+import type { Dispatch, HTMLInputTypeAttribute, SetStateAction } from 'react';
 import { memo } from 'react';
+import { FaEnvelope, FaLock, FaPen, FaPhone } from 'react-icons/fa';
 
+/**
+ * Props.
+ */
 type Props = {
   label: string;
   placeholder: string;
   value: string;
   setValue: Dispatch<SetStateAction<string>>;
   helper: string;
-  isPassword?: boolean;
+  type: HTMLInputTypeAttribute;
 };
 
 /**
@@ -28,18 +35,46 @@ const TextInput = ({
   value,
   setValue,
   helper,
-  isPassword = false,
+  type,
 }: Props) => (
   <FormControl isRequired>
     <FormLabel>{label}</FormLabel>
-    <Input
-      borderColor="gray.400"
-      type={isPassword ? 'password' : 'text'}
-      autoComplete="off"
-      placeholder={placeholder}
-      value={value}
-      onChange={({ currentTarget: { value } }) => setValue(value)}
-    />
+
+    <InputGroup>
+      {type === 'text' && (
+        <InputLeftElement pointerEvents="none">
+          <Icon as={FaPen} color="gray.400" />
+        </InputLeftElement>
+      )}
+
+      {type === 'email' && (
+        <InputLeftElement pointerEvents="none">
+          <Icon as={FaEnvelope} color="gray.400" />
+        </InputLeftElement>
+      )}
+
+      {type === 'tel' && (
+        <InputLeftElement pointerEvents="none">
+          <Icon as={FaPhone} color="gray.400" />
+        </InputLeftElement>
+      )}
+
+      {type === 'password' && (
+        <InputLeftElement pointerEvents="none">
+          <Icon as={FaLock} color="gray.400" />
+        </InputLeftElement>
+      )}
+
+      <Input
+        borderColor="gray.400"
+        type={type}
+        autoComplete="off"
+        placeholder={placeholder}
+        value={value}
+        onChange={({ currentTarget: { value } }) => setValue(value)}
+      />
+    </InputGroup>
+
     <FormHelperText fontSize="xs">{helper}</FormHelperText>
   </FormControl>
 );
