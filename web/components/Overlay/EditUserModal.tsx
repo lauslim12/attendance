93732378562191b7b0fbe804742092965dc21382
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2022-03-02 22:59:05 +0700

    feat: Add a 'Tab' component to allow credential edits

--- web/components/Overlay/EditUserModal.tsx
+++ web/components/Overlay/EditUserModal.tsx
@@ -9,6 +9,11 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
   useToast,
   VStack,
 } from '@chakra-ui/react';
@@ -46,6 +51,10 @@ const EditUserModal = ({ isOpen, onClose, user }: Props) => {
   const [phoneNumber, setPhoneNumber] = useState(user.phoneNumber);
   const [role, setRole] = useState(user.role as string);
 
+  // To edit credentials: username and password.
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   // Reactive component.
   const [isLoading, setIsLoading] = useState(false);
   const toast = useToast();
@@ -55,10 +64,22 @@ const EditUserModal = ({ isOpen, onClose, user }: Props) => {
     e.preventDefault();
     setIsLoading(true);
 
+    // Ensure that the credentials are passed properly.
+    const withUsername = username.trim() !== '' ? username : undefined;
+    const withPassword = password !== '' ? password : undefined;
+
+    // Make a HTTP request.
     axios({
       method: 'PATCH',
       url: `/api/v1/users/${user.userID}`,
-      data: { email, fullName, phoneNumber, role },
+      data: {
+        email,
+        fullName,
+        phoneNumber,
+        role,
+        username: withUsername,
+        password: withPassword,
+      },
     })
       .then((res) => SuccessToast(toast, res.message))
       .catch((err) => FailedToast(toast, err.message))
@@ -83,47 +104,85 @@ const EditUserModal = ({ isOpen, onClose, user }: Props) => {
         <ModalCloseButton />
 
         <ModalBody>
-          <VStack as="form" align="stretch" onSubmit={editUser}>
-            <Alert status="success" variant="left-accent">
-              <AlertIcon />
-              Edit a user here!
-            </Alert>
+          <Tabs isFitted>
+            <TabList>
+              <Tab>Information</Tab>
+              <Tab>Credentials</Tab>
+            </TabList>
 
-            <TextInput
-              label="Full name"
-              placeholder="John Smith"
-              value={fullName}
-              setValue={setFullName}
-              helper="The full name to be used."
-              type="text"
-            />
+            <TabPanels>
+              <TabPanel px="0">
+                <VStack as="form" align="stretch" onSubmit={editUser}>
+                  <Alert status="success" variant="left-accent">
+                    <AlertIcon />
+                    Edit a user here!
+                  </Alert>
 
-            <TextInput
-              label="Email"
-              placeholder="john.smith@gmail.com"
-              value={email}
-              setValue={setEmail}
-              helper="The email to be used."
-              type="text"
-            />
+                  <TextInput
+                    label="Full name"
+                    placeholder="John Smith"
+                    value={fullName}
+                    setValue={setFullName}
+                    helper="The full name to be used."
+                    type="text"
+                  />
 
-            <TextInput
-              label="Phone number"
-              placeholder="081219204571"
-              value={phoneNumber}
-              setValue={setPhoneNumber}
-              helper="The phone number to be used in Indonesian format (with or without +62)."
-              type="tel"
-            />
+                  <TextInput
+                    label="Email"
+                    placeholder="john.smith@gmail.com"
+                    value={email}
+                    setValue={setEmail}
+                    helper="The email to be used."
+                    type="text"
+                  />
 
-            <SelectInput
-              label="Role"
-              value={role}
-              setValue={setRole}
-              options={['admin', 'user']}
-              helper="Role of this user"
-            />
-          </VStack>
+                  <TextInput
+                    label="Phone number"
+                    placeholder="081219204571"
+                    value={phoneNumber}
+                    setValue={setPhoneNumber}
+                    helper="The phone number to be used in Indonesian format (with or without +62)."
+                    type="tel"
+                  />
+
+                  <SelectInput
+                    label="Role"
+                    value={role}
+                    setValue={setRole}
+                    options={['admin', 'user']}
+                    helper="Role of this user"
+                  />
+                </VStack>
+              </TabPanel>
+
+              <TabPanel px="0">
+                <VStack as="form" align="stretch" onSubmit={editUser}>
+                  <Alert status="warning" variant="left-accent">
+                    <AlertIcon />
+                    Caution. You are editing credentials.
+                  </Alert>
+
+                  <TextInput
+                    label="Username"
+                    placeholder="Username"
+                    value={username}
+                    setValue={setUsername}
+                    helper="The username to change to."
+                    type="text"
+                  />
+
+                  <TextInput
+                    label="Password"
+                    placeholder="••••••••••"
+                    value={password}
+                    setValue={setPassword}
+                    helper="The password to change to."
+                    type="password"
+                  />
+                </VStack>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </ModalBody>
 
         <ModalFooter>
